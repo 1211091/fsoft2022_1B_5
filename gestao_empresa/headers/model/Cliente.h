d58fe7bb5761e5b32f18729daa9002e10a0ed50c
1211084 <1211084@isep.ipp.pt>
2022-06-01 15:06:14 +0100

    Models

---
+++ gestao_empresa/headers/model/Cliente.h
@@ -0,0 +1,8 @@
+//
+// Created by bilax on 01/06/2022.
+//
+
+#ifndef MAIN_CPP_CLIENTE_H
+#define MAIN_CPP_CLIENTE_H
+
+#endif //MAIN_CPP_CLIENTE_H
---
+++ gestao_empresa/headers/model/Nome.h
@@ -0,0 +1,20 @@
+//
+// Created by bilax on 01/06/2022.
+//
+
+#ifndef MAIN_CPP_NOME_H
+#define MAIN_CPP_NOME_H
+
+#include <string>
+using namespace std;
+
+class Nome{
+    protected:
+        string nome;
+    public:
+        void setNome(const string &nome);
+
+
+};
+
+#endif //MAIN_CPP_NOME_H
