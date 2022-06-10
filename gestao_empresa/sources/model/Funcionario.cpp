4d006c49c4737cd87b1aa2e25cff2c6b61c48996
1211084 <1211084@isep.ipp.pt>
2022-06-10 13:42:29 +0100

    funcionario

--- gestao_empresa/sources/model/Funcionario.cpp
+++ gestao_empresa/sources/model/Funcionario.cpp
@@ -6,11 +6,28 @@
 
 
 Funcionario::Funcionario(const string& funcao);{
-        setFuncao(funcao);
+    this->setFuncao(funcao);
 }
 Funcionario::Nome(const string& nome){
-    setNome(nome);
+    this-> setNomeFuncionario(nomeFuncionario);
 }
-Funcionario::Funcionario(const string& )
+Funcionario::Funcionario(const Funcionario &obj){
+ this->setFuncao(obj.funcao);
+}
+Funcionario::Nome(const Funcionario &obj)
+
 Funcionario::~Funcionario{
 };
+const Funcionario::getFuncao(){
+    return funcao;
+}
+const Funcionario::getNomeFuncionario(){
+    return nomeFuncionario;
+}
+bool Funcionario::operator ==(const funcao& obj)const{
+    if(*(this->funcao)==*(obj.subject)) {
+        return true;
+    }
+    return false;
+}
+
